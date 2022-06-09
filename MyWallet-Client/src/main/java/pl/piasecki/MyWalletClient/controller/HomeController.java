bcc2339096abc7a61ef45e79c0019f885e4c964a
Ocaru <kamil.piasecki14@gmail.com>
2022-06-09 15:13:04 +0200

    Update default controller

--- MyWallet-Client/src/main/java/pl/piasecki/MyWalletClient/controller/HomeController.java
+++ MyWallet-Client/src/main/java/pl/piasecki/MyWalletClient/controller/HomeController.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 
 import pl.piasecki.MyWalletClient.RestClient;
 import pl.piasecki.MyWalletClient.model.Expenditure;
@@ -13,18 +17,94 @@ import pl.piasecki.MyWalletClient.model.User;
 @Controller
 public class HomeController {
 
+	private List<User> userList;
+	//private List<Expenditure> expenditureList;
+	private Expenditure[] expenditureTab;
+	private RestClient rc = new RestClient();
+	
 	@RequestMapping("/")
 	public String index(Model model) {
-		RestClient rc = new RestClient();
+		//expenditureList = rc.get("/expenditures", Expenditure.class);
+		expenditureTab = rc.getExpenditures("/expenditures");
 
-		List<User> userList = rc.get("/users", User.class);
-		List<Expenditure> expenditureList = rc.get("/expenditures", Expenditure.class);
-
-		model.addAttribute("expenditureList", expenditureList);
-		model.addAttribute("userList", userList);
+		model.addAttribute("expenditureList", expenditureTab);
+		
+		Expenditure expenditure = new Expenditure();
+		model.addAttribute("expenditure", expenditure);
 
 		return "index";
 
 	}
+	
+	@RequestMapping("/userPage")
+	public String showUserPage(Model model)
+	{
+		userList = rc.get("/users", User.class);
+		User theUser = new User();
+		
+		model.addAttribute("user", theUser);
+		model.addAttribute("userList", userList);
+		
+		return "userPage";
+	}
+	
+	@RequestMapping("/saveExpenditure")
+	public String saveNewExpenditure(@ModelAttribute("expenditure") Expenditure expenditure )
+	{
+		JsonMapper mapper = new JsonMapper();
+		String expenditureJSON = ""; 
+		try {
+			expenditureJSON = mapper.writeValueAsString(expenditure);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		rc.post("/expenditures", expenditureJSON);
+		
+		return "redirect:/";
+	}
+	
+	
+
+	
+	@RequestMapping("/updateExpenditure")
+	public String updateExpenditure(@ModelAttribute("expenditure") Expenditure expenditure )
+	{
+		System.out.println("UPDATEEPXENDITURE START");
+		JsonMapper mapper = new JsonMapper();
+		String expenditureJSON = "";
+		try {
+			expenditureJSON = mapper.writeValueAsString(expenditure);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		rc.put("/expenditures", expenditureJSON);
+		
+		return "redirect:/";
+	}
+	
+	
+	@RequestMapping("/setExpenditureToUpdate")
+	public String setExpenditureToUpdate(@ModelAttribute("expenditure") Expenditure expenditure, Model model )
+	{
+		 Expenditure[] exps = rc.getExpenditures("/expenditures");
+		
+		 System.out.println(expenditure.toString()); 
+
+		 System.out.println(exps.toString());
+		 
+		 
+		 for (Expenditure exp : exps) {
+				if(exp.getId() == expenditure.getId())
+				{
+					model.addAttribute("expenditureToUpdate", exp);
+					System.out.println("\t DODANO DO MODELU expenditureToUpdate");
+					System.out.println(exp.toString());
+				} 
+			
+		}
+		 
+		return "/expenditureUpdatePage";
+	}
+	
 
 }
