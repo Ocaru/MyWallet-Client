3efe65e798b00f4ac5a41368aa25be1b6e92de01
Ocaru <kamil.piasecki14@gmail.com>
2022-07-05 23:19:16 +0200

    test: add test for Spring context

--- MyWallet-Client/src/test/java/pl/piasecki/MyWalletClient/WalletApplicationTests.java
+++ MyWallet-Client/src/test/java/pl/piasecki/MyWalletClient/WalletApplicationTests.java
@@ -1,13 +1,31 @@
-package pl.piasecki.Wallet;
+package pl.piasecki.MyWalletClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import pl.piasecki.MyWalletClient.controller.ExpenditureController;
+import pl.piasecki.MyWalletClient.controller.HomeController;
+import pl.piasecki.MyWalletClient.controller.UserController;
 
 @SpringBootTest
 class WalletApplicationTests {
 
+	@Autowired
+	private HomeController homeController;
+	@Autowired
+	private ExpenditureController expenditureController;
+	@Autowired
+	private UserController userController;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(homeController).isNotNull();
+		assertThat(expenditureController).isNotNull();
+		assertThat(userController).isNotNull();
+		
 	}
 
 }
